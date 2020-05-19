448cad5d3d6dd99b2c91fb24e8b084fcbef3c9f7
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-05-19 14:28:23 +0200

    es. completato

---
+++ script.js
@@ -0,0 +1,28 @@
+var source   = document.getElementById("quadrato-template").innerHTML;
+var template = Handlebars.compile(source);
+
+for (var i = 0; i < 36; i++) {
+    $('.griglia').append(template())
+}
+
+$('.quadrato').click(function(){
+    quadrato_corrente = $(this)
+
+    $.ajax({
+        'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+        'method': 'GET',
+        'success': function(data) {
+            var numero_random = data.response;
+            quadrato_corrente.children('.numero').text(numero_random)
+            if (numero_random <= 5) {
+                quadrato_corrente.addClass('giallo')
+            }
+            else {
+                quadrato_corrente.addClass('verde')
+            }
+        },
+        'error': function() {
+            alert('si è verificato un errore');
+        }
+    });
+})
